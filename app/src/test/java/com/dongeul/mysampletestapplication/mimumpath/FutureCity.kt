5a5e7703d5a810febc0cafb2570ca8c4b3068684
youeuneyol <ey.yoo@fanmaum.com>
2022-05-26 18:52:20 +0900

    다익스트라

---
+++ app/src/test/java/com/dongeul/mysampletestapplication/mimumpath/FutureCity.kt
@@ -0,0 +1,61 @@
+package com.dongeul.mysampletestapplication.mimumpath
+
+import org.junit.Test
+
+
+class FutureCity {
+// 다익스트라
+
+    //https://velog.io/@cksgodl/Kotlin%EC%97%90%EC%84%9C%EC%9D%98-%EB%8B%A4%EC%9D%B5%EC%8A%A4%ED%8A%B8%EB%9D%BC-%EA%B5%AC%ED%98%84
+
+    @Test
+    fun `미래도시`(){
+        val totalCompanyCount = 5 //n
+        val pathCount = 7 // m
+
+        //목표는 x 회사 = 4
+
+        // 소개팅 상대 k 회사 =5
+
+
+        //도달 x = -1
+
+
+        // 1 { 2,3,4}
+
+        // 2 { 4 }
+
+        // 3 { 4,5}
+
+        // 4 { 5}
+
+        // 목표는 K    + k -> x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
